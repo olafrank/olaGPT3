d6758fab780ba2c69b3139b6c5eabf210036d4cd
olafrank <olafrnk@gmail.com>
2022-12-24 01:24:16 +0100

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) =>{
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://olagpt3.onrender.com',{
     method: 'POST',
     headers:{
       'content-type': 'application/json'
